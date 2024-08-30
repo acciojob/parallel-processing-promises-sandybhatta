45abb68ff6a97f7fceb5f79c4813a03c42af1ff3
sandybhatta <105071143+sandybhatta@users.noreply.github.com>
2024-08-30 11:31:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener("click", () => {
+  fetchingImages();
+});
+
+async function fetchingImages() {
+  try {
+    const imagePromises = images.map((image) =>
+      fetch(image.url).then((response) => {
+        if (!response.ok) {
+          throw new Error(`Failed to load image's URL: ${image.url}`);
+        }
+        return response.blob();
+      })
+    );
+
+    const blobs = await Promise.all(imagePromises);
+    updateUI(blobs);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+function updateUI(blobs) {
+  output.innerHTML = "";
+  blobs.forEach((blob) => {
+    const img = document.createElement("img");
+    img.src = URL.createObjectURL(blob);
+    output.appendChild(img);
+  });
+}
